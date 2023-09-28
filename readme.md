f727c3165e0b6b6b01d76e95702d539250493bd7
Daniel de Moura Silva <nazuyamikima@gmail.com>
2023-09-28 15:03:59 -0300

    :tada: initial commit

---
+++ readme.md
@@ -0,0 +1,22 @@
+# Game Detona Ralph
+
+<p align="center">
+  <img src="./.github/preview.gif" alt="Game Detona Ralph Logo">
+</p>
+
+Esse jogo foi desenvolvido durante o Bootcamp: Potência Tech iFood - Desenvolvimento de Jogos
+
+### Tecnologias Utilizadas
+
+- HTML5 e CSS3:
+  - Para a estrutura e aparência do jogo.
+- JavaScript:
+  - Para a lógica de programação e interatividade.
+- Sprites e imagens:
+  - Customizadas para criar a estética do jogo.
+
+### Funcionalidades
+
+- **Sistema de Pontuação**: Acompanhe sua pontuação à medida que acerta o alvo.
+
+- **Sistema de Vidas**: Você tem 3 vidas, depois que tiver acabado o seu tempo você perde uma vida, e quando você perder todas as suas 3 vidas o jogo termina.
---
+++ src/scripts/engine.js
@@ -0,0 +1,81 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector("#enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+        lives: document.querySelector(".lives"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 30,
+        lives: 3,
+    },
+    actions: {
+        timeId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0) {
+       
+        alert("GAME OVER! Seu tempo acabou a sua pontuação foi " + state.values.result);
+        state.values.currentTime = 30;
+        state.values.lives--;
+        state.values.result = 0;
+
+        state.view.score.textContent = state.values.result;
+        state.view.lives.textContent = "x" + state.values.lives;
+    }
+
+    if(state.values.lives === 0) {
+        clearInterval(state.actions.timeId);
+        clearInterval(state.actions.countDownTimerId);
+    }
+}
+
+function playSound() {
+    let audio = new Audio("./src/audios/hit.m4a");
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    })
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+
+    state.values.hitPosition = randomSquare.id;
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach(square => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                if(state.values.lives > 0) {
+                    state.values.result++
+                    state.view.score.textContent = state.values.result;
+                    state.values.hitPosition = null;
+                    playSound();                
+                }
+            }
+        })
+    })
+}
+
+function initialize() {
+    addListenerHitBox();
+}
+
+initialize()
